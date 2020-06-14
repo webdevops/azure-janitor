a1a19267a2013369de3f620a4f378e053f251242
Markus Blaschke <mblaschke82@gmail.com>
2020-06-14 12:36:24 +0200

    Ensure lowercase resourcegroup name

--- misc.go
+++ misc.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"regexp"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -14,7 +13,7 @@ var (
 
 func extractResourceGroupFromAzureId(azureId string) (resourceGroup string) {
 	if subMatch := resourceGroupFromResourceIdRegExp.FindStringSubmatch(azureId); len(subMatch) >= 1 {
-		resourceGroup = subMatch[1]
+		resourceGroup = strings.ToLower(subMatch[1])
 	}
 
 	return
